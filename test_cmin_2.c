371faad5127ec999b9173cc955b67f32c63f2d7b
BenjiCooper <benji97@gmail.com>
2017-09-25 14:19:41 -0400

    modified test 2

--- test_cmin_2.c
+++ test_cmin_2.c
@@ -4,6 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 void crash1() {
 	int *x = NULL;
@@ -28,10 +32,20 @@ int main(int argc, char **argv) {
 
 	if (argc != 2) {
 		printf("insufficient arguments\n");
-		return -1;
+		return 1;
 	}
 
-	n = argv[1][0] + '0';
+	int fd = open(argv[1], O_RDONLY);
+  	if (fd == -1)
+  	  return 1;
+
+  	int r = read(fd, &n, 1);
+  	close(fd);
+
+  	if (r != 1)
+  		return 1;
+
+	n = n + '0';
 	n = foo(n);
 	n = bar(n);
 
