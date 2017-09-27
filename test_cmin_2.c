ffaaf57604471c5dd0555d4be23df9faabf86fda
BenjiCooper <benji97@gmail.com>
2017-09-27 14:34:00 -0400

    updated cmin_2

--- test_cmin_2.c
+++ test_cmin_2.c
@@ -14,7 +14,7 @@ void crash1() {
 }
 
 void crash2() {
-	printf("%d\n", *((volatile int *)1));
+	printf("%d\n", *((volatile char *) 0));
 }
 
 int foo(int x) {
@@ -26,36 +26,37 @@ int bar(int x) {
 }
 
 int main(int argc, char **argv) {
-	int n;
+	char c;
+	int n, x;
 
 	if (argc != 2) {
-		printf("insufficient arguments\n");
+		printf("incorrect number of arguments!\n");
 		return 1;
 	}
 
 	int fd = open(argv[1], O_RDONLY);
   	if (fd == -1)
-  	  return 1;
+    	return 1;
 
-  	int r = read(fd, &n, 1);
+    int r = read(fd, &c, 1);
   	close(fd);
 
-  	if (r != 1)
+  	if (r!=1)
   		return 1;
 
-	n = n + '0';
-	n = foo(n);
-	n = bar(n);
-
-	if (n%3 == 0) {
-		printf("%d\n", 1);
-		crash1();
-	} else if (n%3 == 1) {
-		printf("%d\n", 2);
-		crash2();
-	} else {
-		printf("success!\n");
-	}
-
-	return 0;
+  	n = c-'0';
+  	n = foo(n);
+  	n = bar(n);
+  	if (n%3 == 0) {
+  		printf("1\n");
+  		crash1();
+  	} else if (n%3 == 1) {
+  		printf("2\n");
+  		crash2();
+  	} else {
+  		printf("success!\n");
+  	}
+  	return 0;
 }
+
+
